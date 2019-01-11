71b209c9544c674c41b0a735152386882ef21a01
Michael <michael@managedkaos.com>
2019-01-10 20:28:12 -0800

    funky state

--- Jenkins/wait_for_jenkins_login.sh
+++ Jenkins/wait_for_jenkins_login.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-until [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://127.0.0.1:8484) -eq 403 ]; do echo -n '.'; sleep 1; done
+#!/bin/sh
+until [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://jenkins-server:8484) -eq 403 ]; do echo -n '.'; sleep 1; done
