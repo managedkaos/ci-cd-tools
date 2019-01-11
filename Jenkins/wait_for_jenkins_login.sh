602d33e8c97abc3970a48dea5e0661954f627a12
Michael <michael@managedkaos.com>
2019-01-10 22:10:01 -0800

    use docker-compose to get the password

--- Jenkins/wait_for_jenkins_login.sh
+++ Jenkins/wait_for_jenkins_login.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 until [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://localhost:8484) -eq 403 ]; do echo -n '.'; sleep 1; done
 echo
-docker logs jenkins-server 2>&1| grep -A 2 "Please use the following password"
+docker-compose logs | grep -A 2 "Please use the following password"
